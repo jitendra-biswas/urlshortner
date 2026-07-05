e924bd9361e827b690709f054c7cb55e43a50e6c
jitendra-biswas <jitendrabiswas12344@gmail.com>
2026-07-05 08:57:16 +0545

    fix mongodb connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async function(){
-    await mongoose.connect('mongodb://localhost:27017/URLShortner');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongodb connected!");
 }
 
